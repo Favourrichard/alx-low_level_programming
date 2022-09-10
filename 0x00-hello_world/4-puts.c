a940a85c116563d35f52755eef47a6a3d9927bc2
Favourrichard <favourrichard004@gmail.com>
2022-09-10 11:12:39 -0700

    Task 4 puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
- * main - print the string in the put function
- *
- * Description: using the main function
- * Return: 0 if success.
- */
-init main(void)
+* main - Printing
+*
+* Return: Always 0 (Successful)
+*/
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual
+puzzle");
 return (0);
 }
